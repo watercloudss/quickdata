9ea9fb66101202546cebc9bf44f051e68e9eff9d
lly <liuliyuan666@aliyun.com>
2019-05-27 10:00:38 +0800

    Initial commit

---
+++ src/main/java/com/springboot/study/QuickdataApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickdataApplication.class, args);
+    }
+
+}
